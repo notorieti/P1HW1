b6d4a44f4b8ef3bb08a5125212c059435029b07e
notorieti <smithd4829@student.faytechcc.edu>
2025-02-13 14:41:18 -0500

    Add files via upload

---
+++ P1HW1_DavinSmith.py
@@ -0,0 +1,36 @@
+# Davin Smith
+ # 2-16-25
+ # Assignment Name: P1HW2
+ # A brief description of the project
+import math 
+# \n is the newline character
+
+print("\n-----Calculating Exponents-----\n\n")
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+print("\n")
+value = base**exponent
+value = pow(base, exponent)
+#value = math.pow(base, exponent)
+print(base,"raised to the power of",exponent,"is",value,"!!")
+
+
+
+
+
+
+
+
+
+##print("-"*20,"Calculating Exponents","-"*20)
+
+ 
+ 
+##num1 = int(input("Enter a number: "))
+##num2 = int(input("Enter another number: "))
+##
+##num3 = num1 + num2
+##num3 = str(num3)
+##print(num1, "+",num2,"=", num3)
+##
+##input("Press any key to continue!!")
